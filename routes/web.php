cf973a6370ba65aa1cb9f5f190079207a3958492
Gabriel Guzman <gabo.icaro@gmail.com>
2023-07-18 22:24:40 -0500

    Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,30 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/',HomeController::class);
 
+Route::get('cursos',[CursoController::class,'index']);
+
+Route::get('cursos/create',[CursoController::class,'create']);
+/*
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return "Inicio";
 });
+Route::get('cursos', function () {
+    //return view('welcome');
+    return "Cursos";
+});
+Route::get('cursos/create', function () {
+    return "Crear Cursos: ";
+});
+
+Route::get('cursos/{id_curso}/{id_categoria?}', function ($id_curso,$id_categoria= null) {
+    
+    if($id_categoria){
+        return "Cursos: " . $id_curso." Categoria".$id_categoria;
+    }else{
+        return "Cursos: " . $id_curso;
+    }
+    
+});*/
